4333f8a5e67c6127baa5cd6df53cd9c490e5282d
Jakub Czyz <jakub12134@gmail.com>
2023-10-24 14:51:53 +0200

    update bucket name

--- infra/05-storage.tf
+++ infra/05-storage.tf
@@ -1,6 +1,6 @@
 ### Storage Bucket for Static Files (web app)
 resource "google_storage_bucket" "public" {
-  name                        = "sb-${local.props.project_name}-webcomponent"
+  name                        = "sb-${data.google_project.main.project_id}-webcomponent"
   location                    = local.props.gcp.location
   public_access_prevention    = "inherited"
   storage_class               = "STANDARD"
